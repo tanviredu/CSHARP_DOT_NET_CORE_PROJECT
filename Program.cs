b27fa6f5b7d253c9172b104b916fa46d09fb0999
Tanvir Rahman <tanvir@localhost.localdomain>
2019-12-13 10:44:25 +0000

    added

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSHARP_DOT_NET_CORE_PROJECT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("HEllo world again");
+        }
+    }
+}
